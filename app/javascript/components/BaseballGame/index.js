16fd2b78283c8f807155a3a924ce3dd8ace10fd6
Andrew Sndier <andrewsnider15@gmail.com>
2023-05-02 16:18:22 -0700

    add basic outcomes

--- app/javascript/components/BaseballGame/index.js
+++ app/javascript/components/BaseballGame/index.js
@@ -23,7 +23,84 @@ class BaseballGame extends Component {
   }
 
   throwPitch = () => {
-    console.log('I throwed a pitch')
+    const { balls, strikes, outs, latestOutcome } = this.state;
+    const possibleOutcomes = ['Ball', 'Strike', 'Foul ball', 'In play, out(s)', 'In play, no out', 'In play, run(s)']
+    const outcome = this.decideOutcome(possibleOutcomes)
+    // console.log(outcome)
+    if (outcome === 'Ball') {
+        this.handlePitchedBall(outcome);
+    }
+    else if (outcome === 'Strike') {
+        this.handlePitchedStrike(outcome);
+    }
+    else if (outcome === 'Foul ball') {
+        this.handleFoulBall(outcome);
+    }
+    else if (outcome === 'In Play, out(s)') {
+        this.handleInPlayOuts(outcome);
+    }
+    else if (outcome === 'In play, no out') {
+        this.handleInPlayNoOut(outcome);
+    }
+    else if (outcome === 'In play, run(s)') {
+        this.handleInPlayRuns(outcome);
+    }  
+  }
+
+  decideOutcome = possibleOutcomes => {
+    const randomIndex = Math.floor(Math.random() * possibleOutcomes.length);
+    return possibleOutcomes[randomIndex];
+  }
+
+  handlePitchedBall = outcome => {
+    this.setState(prevState => ({
+        balls: this.state.balls < 4 ? this.state.balls + 1 : 0,
+        latestOutcome: outcome, 
+        allOutcomes: [...prevState.allOutcomes, outcome]
+      }));
+    }
+
+  handlePitchedStrike = outcome => {
+    this.setState(prevState => ({
+        strikes: this.state.strikes < 3 ? this.state.strikes + 1 : 0, 
+        latestOutcome: outcome, 
+        allOutcomes: [...prevState.allOutcomes, outcome]
+      }));
+  }
+
+  handleFoulBall = outcome => {
+    this.setState(prevState => ({
+        strikes: this.state.strikes < 2 ? this.state.strikes + 1 : 2,
+        latestOutcome: outcome, 
+        allOutcomes: [...prevState.allOutcomes, outcome]
+      }));
+  }
+  
+  handleInPlayNoOut = outcome => {
+    this.setState(prevState => ({
+        balls: 0,
+        strikes: 0,
+        latestOutcome: outcome, 
+        allOutcomes: [...prevState.allOutcomes, outcome]
+      }));
+  }
+  
+  handleInPlayOuts = outcome => {
+    this.setState(prevState => ({
+        balls: 0,
+        strikes: 0,
+        latestOutcome: outcome, 
+        allOutcomes: [...prevState.allOutcomes, outcome]
+      }));
+  }
+
+  handleInPlayRuns = outcome => {
+    this.setState(prevState => ({
+        balls: 0,
+        strikes: 0,
+        latestOutcome: outcome, 
+        allOutcomes: [...prevState.allOutcomes, outcome]
+      }));
   }
 
   render() {
@@ -38,6 +115,8 @@ class BaseballGame extends Component {
         latestOutcome,
         allOutcomes
     } = this.state;
+
+    console.log(allOutcomes)
 
     return (
         <>
